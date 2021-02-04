88008f515420d60a3c82bcf86cc9aaaa0f0f20e0
Ramon Bautista <bautramo@amazon.com>
2021-02-03 19:11:51 -0500

    Check if repo is public

--- cli/aws_orbit/docker.py
+++ cli/aws_orbit/docker.py
@@ -43,12 +43,11 @@ def dockerhub_pull(name: str, tag: str = "latest") -> None:
 
 
 def ecr_pull(manifest: "Manifest", name: str, tag: str = "latest") -> None:
-    ecr_address = f"{manifest.account_id}.dkr.ecr.{manifest.region}.amazonaws.com"
-    sh.run(f"docker pull {ecr_address}/{name}:{tag}")
-
-
-def ecr_pull_public(name: str, tag: str = "latest") -> None:
-    sh.run(f"docker pull {name}:{tag}")
+    if name.startswith("public.ecr.aws"):
+        sh.run(f"docker pull {name}:{tag}")
+    else:
+        ecr_address = f"{manifest.account_id}.dkr.ecr.{manifest.region}.amazonaws.com"
+        sh.run(f"docker pull {ecr_address}/{name}:{tag}")
 
 
 def ecr_pull_external(manifest: "Manifest", repository: str, tag: str = "latest") -> None:
